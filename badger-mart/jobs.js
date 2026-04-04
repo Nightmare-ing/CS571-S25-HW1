c9822e530267206f57506c8e2395e656f14ee1e3
Qingsong Hu <kingsanhu@gmail.com>
2026-04-04 20:48:55 +0800

    Finish part 8: Applying for a job

--- badger-mart/jobs.js
+++ badger-mart/jobs.js
@@ -1,5 +1,11 @@
 function submitApplication(e) {
     e.preventDefault(); // You can ignore this; prevents the default form submission!
 
-    // TODO: Alert the user of the job that they applied for!
+    for (job of document.getElementsByName("job")) {
+        if (job.checked === true) {
+            alert(`Thank you for applying to be a ${job.value}`);
+            return;
+        }
+    }
+    alert("Please select a job!");
 }
